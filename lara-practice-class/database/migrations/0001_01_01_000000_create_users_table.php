7e38e30cf4eb00dbc54e04fae8e377e5ada2c3bb
sohelr-dev <sohe.4273@gmail.com>
2025-10-26 12:58:50 +0600

    factory,migration,seeders

--- lara-practice-class/database/migrations/0001_01_01_000000_create_users_table.php
+++ lara-practice-class/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->integer('role_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
