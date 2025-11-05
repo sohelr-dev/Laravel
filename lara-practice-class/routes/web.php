5b3b6f44e01a721c6833c775d36a70b3a073a0d5
sohelr-dev <sohe.4273@gmail.com>
2025-11-05 11:51:12 +0600

    role base route set

--- lara-practice-class/routes/web.php
+++ lara-practice-class/routes/web.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\StatusController;
 use App\Http\Controllers\TraineeController;
 use App\Http\Controllers\UserController;
 use App\Mail\RegisterConfirmationMail;
+use App\Models\Role;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\Group;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,17 +32,48 @@ Route::middleware('auth')->group(function () {
     // Route::get('/roles',[RoleController::class,'index'])->name("roles.index");
     // Route::get('/roles/role/{id}',[RoleController::class,'show'])->name("roles.show");
 
-    // Route::get("/users",[UserController::class,'index'])->name("users.index");
-    // Route::get("/users/create",[UserController::class,'create'])->name("users.create");
-    // Route::get("/users/edit/{id}",[UserController::class,'edit'])->name("users.edit");
-    // Route::patch("/users/{id}",[UserController::class,'update'])->name("users.update");
-    // Route::post("/users",[UserController::class,'store'])->name("users.store");
-    // Route::get("/users/user/{id}",[UserController::class,'show'])->name("user.show");
-    // Route::delete("/users/user/{id}",[UserController::class,'destroy'])->name("user.destroy");
+    Route::get("/users",[UserController::class,'index'])->name("users.index");
+    // Route::get("/users",[UserController::class,'index'])->middleware('role:2,3')->name("users.index");   //single middleware
+    Route::get("/users/create",[UserController::class,'create'])->name("users.create");
+    Route::get("/users/edit/{id}",[UserController::class,'edit'])->name("users.edit");
+    Route::patch("/users/{id}",[UserController::class,'update'])->name("users.update");
+    Route::post("/users",[UserController::class,'store'])->name("users.store");
+    Route::get("/users/user/{id}",[UserController::class,'show'])->name("user.show");
+    Route::delete("/users/user/{id}",[UserController::class,'destroy'])->name("user.destroy");
+
+    //by grouping
+
+    // Route::middleware('role:1,3')->group(function (){
+    //     Route::get("/users/create",[UserController::class,'create'])->name("users.create");
+    //     Route::get("/users/edit/{id}",[UserController::class,'edit'])->name("users.edit");
+    //     Route::patch("/users/{id}",[UserController::class,'update'])->name("users.update");
+    //     Route::post("/users",[UserController::class,'store'])->name("users.store");
+    //     Route::get("/users/user/{id}",[UserController::class,'show'])->name("user.show");
+    //     Route::delete("/users/user/{id}",[UserController::class,'destroy'])->name("user.destroy");
+    //     Route::get('/roles',[RoleController::class,'index'])->name("roles.index");
+    //     Route::get('/roles/role/{id}',[RoleController::class,'show'])->name("roles.show");
+        
+    // });
+
+    //another grouping indivisul controller not middleware
+
+    // Route::controller(UserController::class)->group(function (){
+    //     Route::get('/users','index')->name('users.index');
+    //     Route::get('/users/create','create')->name('users.create');
+    //     Route::get('/users/edit/{id}','edit')->name('users.edit');
+    //     Route::get('/users/user/{id}','show')->name('users.show');
+    //     Route::post("/users",'store')->name("users.store");
+
+
+    // });
+
+
 
     Route::resource('roles',RoleController::class);
-    Route::resource('users',UserController::class);
+    // Route::resource('users',UserController::class);
     Route::resource('statuses',StatusController::class);
+
+    // Route::resource('users',UserController::class)->middleware('role:2,3');
 });
 
 // web0
