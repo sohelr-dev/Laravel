7f4b3a0fcac0cf2aa9a41443f7726b4a36fd0a12
sohelr-dev <sohe.4273@gmail.com>
2025-11-22 13:13:13 +0600

    update to api connected to react
    A

---
+++ lara-api-project/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Post::all());
+    }
+
+    public function getUser(){
+        return response()->json(Post::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return response()->json(Post::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ lara-api-project/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get('/test',function(){
+//     return 'api is working';
+// });
+
+Route::post('/register',[ApiController::class,'register']);
+Route::post('/login',[ApiController::class,'login']);
+Route::get('/users',[ApiController::class,'getUser']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiresource('/posts',PostController::class);
+    Route::post('/logout',[ApiController::class,'logout']);
+
+});
+
